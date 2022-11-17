a7d35ed4f0e1cb414101e371dfab0f99d0c927c4
Kuba Sobecki <gosustyle@gmail.com>
2022-11-17 18:38:02 +0100

    Use React.memo() and useCallback to prevent unnecessary re-renders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
@@ -7,15 +8,15 @@ function App() {
 
     console.log('APP RUNNING!');
 
-    const toggleParagraphHandler = () => {
+    const toggleParagraphHandler = useCallback(() => {
         setShowParagraph(prevShowParagraph => !prevShowParagraph);
-    };
+    }, []);
 
     return (
         <div className="app">
             <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
             <h1>Hi there!</h1>
-            {showParagraph && <p>This is new!</p>}
+            <DemoOutput show={false} />
         </div>
     );
 }
